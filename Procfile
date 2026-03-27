c1b9a90cf7627c169f2a4e86da04bbeeac3143a5
Sidd-Tiwari <tiwarisid022018@gmail.com>
2026-03-27 11:51:44 +0530

    fixed build, added Procfile, ready for railway deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/school-erp-1.0.0.jar
--- src/test/java/com/springdale/erp/ErpApplicationTests.java
+++ src/test/java/com/springdale/erp/ErpApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springdale;
+package com.springdale.erp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
